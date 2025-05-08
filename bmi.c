b8f0593027157d7bc5b8f4eb41364d6f790fd79a
Facundo Manuel Minatto <facuminatto@gmail.com>
2025-05-08 11:08:57 -0300

    Lo hice en el colectivo, creo que esta bien.

---
+++ bmi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+float peso;
+float altura;
+float BMI;
+
+    printf("Esta es una calculadora de indice de masa corporal\n");
+    printf("Ingrese su peso en kg: ");
+    scanf("%f", &peso);
+    
+    printf("Ahora coloque su estatura en metros: ");
+    scanf("%f", &altura);
+    
+    BMI = peso / (altura * altura);
+    
+    printf("Su BMI es de %.2f", BMI);
+    
+    
+    if (BMI <= 18.5)
+    printf(", esto indica que tiene bajo peso");
+    
+    if (18.5 < BMI)
+     if  (BMI < 24.9)
+      printf(", esto indica que su condicion es normal");
+    
+    if (25 < BMI)
+     if (BMI < 29.9)
+      printf(", esto indica que tiene sobrepeso");
+    
+    if (30 <= BMI)
+    printf(", esto indica que usted tiene obesidad");
+    
+    return 0;
+}
