3a326cef4746051b4ba04a0f3e99c6c58e06e358
Facundo Manuel Minatto <facuminatto@gmail.com>
2025-09-03 09:16:36 -0300

    Se agrego a la impresion final, el calculo de la diagonal.
    A la función que imprime se le agrego el calculo de la diagonal solo si se elige calcular el rectángulo. 
    Y se acomodo un poco mejor el codigo.

--- tp6.c
+++ tp6.c
@@ -4,62 +4,22 @@
 
 float calcularAreaCirculo (float rad);
 
-float calcularAreaCirculo (float rad)
-{
-	float resarea;
-	resarea = (rad * rad) * pi;
-	return resarea;
-}
-
 float calcularPerimetroCirculo (float rad);
-
-float calcularPerimetroCirculo (float rad)
-{
-	float resper;
-	resper = 2 * pi * rad;  
-	return resper;
-}
 
 float calcularAreaRectangulo (float alto, float ancho);
 
-float calcularAreaRectangulo (float alto, float ancho)
-{
-	float resarea;
-	resarea = alto * ancho;
-	return resarea;
-}
-
 float calcularPerimetroRectangulo (float alto, float ancho);
-
-float calcularPerimetroRectangulo (float alto, float ancho)
-{
-	float resper;
-	resper = alto * 2 + ancho * 2;
-	return resper;
-}
 
 float calcularDiagonalRectangulo (float alto, float ancho);
 
-float calcularDiagonalRectangulo (float alto, float ancho)
-{
-	float resdiag;
-	resdiag = sqrt((alto*alto) + (ancho*ancho));
-	return resdiag;
-}
+void imprimirResultado (float resarea, float resper, float resdiag);
 
-void imprimirResultado (float resarea, float resper);
-
-void imprimirResultado (float resarea, float resper)
-{
-	printf("\nEl Area es de: %f \n", resarea);
-	printf("El Perimetro es de: %f \n", resper);
-}
 
 
 int main()
 {
 	int fig;
-	float rad, alto, ancho, resarea, resper, resdiag;
+	float rad, alto, ancho, resarea, resper, resdiag = 0;
 	
 	printf("Bienvenido a la calculadora de Area y perimetro!\n");
 	printf("¿Que figura desea calcular?\n\n");
@@ -73,7 +33,6 @@ int main()
 		printf("\n¿Que figura desea calcular?\n\n");
 		printf("| 1 | Rectangulo.\n| 2 | Circulo.\n\n");
 		scanf("%d", &fig);
-		
 	}
 	
 	switch(fig){
@@ -106,7 +65,54 @@ int main()
 		break;
 	}
 	
-	imprimirResultado (resarea, resper);
+	imprimirResultado (resarea, resper, resdiag);
 	
 	return 0;
 }
+
+
+
+float calcularAreaCirculo (float rad)
+{
+	float resarea;
+	resarea = (rad * rad) * pi;
+	return resarea;
+}
+
+float calcularPerimetroCirculo (float rad)
+{
+	float resper;
+	resper = 2 * pi * rad;  
+	return resper;
+}
+
+float calcularAreaRectangulo (float alto, float ancho)
+{
+	float resarea;
+	resarea = alto * ancho;
+	return resarea;
+}
+
+float calcularPerimetroRectangulo (float alto, float ancho)
+{
+	float resper;
+	resper = alto * 2 + ancho * 2;
+	return resper;
+}
+
+float calcularDiagonalRectangulo (float alto, float ancho)
+{
+	float resdiag = 0;
+	resdiag = sqrt((alto*alto) + (ancho*ancho));
+	return resdiag;
+}
+
+void imprimirResultado (float resarea, float resper, float resdiag)
+{
+	printf("\nEl Area es de: %f \n", resarea);
+	printf("El Perimetro es de: %f \n", resper);
+	
+	if (resdiag != 0)
+		printf("La diagonal de: %f \n", resdiag);	
+}
+
