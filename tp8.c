651c3367f2cda96ca96ed2b4abc73c0294ac872f
Facundo Manuel Minatto <facuminatto@gmail.com>
2025-10-14 09:00:34 -0300

    Actualización tp8.c
    Se agrego un "=" en el do-while para no aceptar diametros que sean o valgan 0.

--- tp8.c
+++ tp8.c
@@ -38,9 +38,9 @@ int main (void) {
 	
 	mostrar_piezas("rechazadas", p_rechazadas, n_rechazar);
 	
-    free(p_rectificables);
-    free(p_rechazadas);
-    
+	free(p_rectificables);
+	free(p_rechazadas);
+	
 	return 0;
 }
 
@@ -71,58 +71,58 @@ int rechazada(float diametro, float valor) {
 void cargar_piezas(float *p, int n){
 	
 	for (int i = 0; i < n; i++){
-    
-        do {
-            printf("Ingrese el diámetro de la pieza n° %d: ", i + 1);
-            scanf("%f", (p + i));
-            
-            if (*(p + i) < 0){
-                printf("ERROR. Ese diámetro no es positivo.\n");
-	    		printf("Ingrese un diámetro positivo.\n");
-			}
-        } while (*(p + i) < 0);
-	}
-}
-	
-int contar(int (*criterio)(float, float), float valor, float *p, int n){
-	
-	int cont = 0;
-	
-	for (int i = 0; i < n; i++){
 		
-		if ( (*criterio)(*(p + i), valor) == 1 )
-		cont++;
+		do {
+			printf("Ingrese el diámetro de la pieza n° %d: ", i + 1);
+			scanf("%f", (p + i));
+			
+			if (*(p + i) <= 0){
+				printf("ERROR. Ese diámetro no es positivo.\n");
+				printf("Ingrese un diámetro positivo.\n");
+			}
+		} while (*(p + i) <= 0);
 	}
-	return cont;
 }
 	
-float* reservar_memoria(int n){
-	
-	float *p = malloc(n * sizeof(float));
-	
-	return p;        // Falta que pueda chequear si se pidió coeeegamela memoria.
-}
-	
-void segregar(int (*criterio)(float, float), float valor, float *p, int n, float *q) {
- 
-    int var = 0;
-    
-    for (int i = 0; i < n; i++) {
-      
-        if (criterio(*(p + i), valor) == 1) {
-        
-            *(q + var) = *(p + i);
-            var++;
-        }
-    }
-}
-
-void mostrar_piezas(char *mensaje, float *p, int n){
-
-    printf("Piezas %s:\n", mensaje);
-    
-    for (int i = 0; i < n; i++) {
-    
-    printf("Diámetro: %.2f\n", *(p + i));
-    }
-}
+	int contar(int (*criterio)(float, float), float valor, float *p, int n){
+		
+		int cont = 0;
+		
+		for (int i = 0; i < n; i++){
+			
+			if ( (*criterio)(*(p + i), valor) == 1 )
+				cont++;
+		}
+		return cont;
+	}
+		
+		float* reservar_memoria(int n){
+			
+			float *p = malloc(n * sizeof(float));
+			
+			return p;        // Falta que pueda chequear si se pidió coeeegamela memoria.
+		}
+			
+			void segregar(int (*criterio)(float, float), float valor, float *p, int n, float *q) {
+				
+				int var = 0;
+				
+				for (int i = 0; i < n; i++) {
+					
+					if (criterio(*(p + i), valor) == 1) {
+						
+						*(q + var) = *(p + i);
+						var++;
+					}
+				}
+			}
+			
+			void mostrar_piezas(char *mensaje, float *p, int n){
+				
+				printf("Piezas %s:\n", mensaje);
+				
+				for (int i = 0; i < n; i++) {
+					
+					printf("Diámetro: %.2f\n", *(p + i));
+				}
+			}
